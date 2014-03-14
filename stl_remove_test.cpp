9ffc12119a0af242d3beef5e6f60d364e4b7ff1d
黄金强 <hellojinqiang@gmail.com>
2014-03-14 09:02:20 +0800

    Create stl_remove_test.cpp

---
+++ stl_remove_test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	list<int> list_int;
+	int element;
+
+	while(cin >> element)
+	{
+		list_int.push_front(element);
+		list_int.push_back(element);
+	}	
+	cin.clear();
+	cin.sync();
+
+	cout << "pre:  ";
+	copy(list_int.begin(), list_int.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	cout << "please input remove element: "; 	
+	cin >> element;
+	remove(list_int.begin(), list_int.end(), element);
+
+	cout << "post: ";
+	copy(list_int.begin(), list_int.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	return 0;
+}
