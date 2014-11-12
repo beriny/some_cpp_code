bae9e9014d231077fa6cad235e71658bb66db621
huangjinqiang <hellojinqiang@gmail.com>
2014-11-13 02:15:20 +0800

    add new content

--- cpp_lambda/lambda.cpp
+++ cpp_lambda/lambda.cpp
@@ -107,5 +107,23 @@ int main(void)
 	a = a.substr(index);
 	b = b.substr(index);
 
+	auto vecta = split(a, '/');
+	auto vectb = split(b, '/');
+
+	string a_path_b = ""; //a relative to b path
+	string b_path_a = ""; //b relative to a path
+
+	for (int i=1; i<vecta.size(); ++i)
+		a_path_b += "../";
+
+	a_path_b += a;
+
+	for(int i=1; i<vectb.size(); ++i)
+		b_path_a += "../";
+	b_path_a += b;
+
+	cout << "a relative to b path: " << a_path_b << endl;
+	cout << "b relative to a path: " << b_path_a << endl;
+
 	return 0;
 }
