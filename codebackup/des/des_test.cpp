5ed36fa527ecca6f515e885c44b7b12fe83d76ff
huangjinqiang <hellojinqiang@gmail.com>
2014-11-23 11:04:27 +0800

    update

---
+++ codebackup/des/des_test.cpp
@@ -0,0 +1,58 @@
+/*************************************************************************
+	>    File Name: des_test.cpp
+	>       Author: huangjinqiang
+	>        Email: ligelaige@gmail.com
+	> Created Time: Tue 04 Nov 2014 11:27:19 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include "des.h"
+
+#define SIZE 128
+
+unsigned getaline(char* str)
+{
+	char c='\n';
+	unsigned i = 0;
+	do
+	{
+		c = getchar();
+		str[i++] = c;
+	} while(c != '\n');
+	
+	str[i-1] = '\0';
+	return i-1;
+}
+
+void writeline(const char a[], const char b[])
+{
+	printf("%s: [", a);
+	printf("%s", b);
+	printf("]\n");
+}
+
+int main(void)
+{
+	char key[8]={1,9,8,0,9,1,7};
+	char str[SIZE];
+	Des_SetKey(key);
+	
+	printf("Enter a string(<=128): ");
+	unsigned length = getaline(str);
+
+	writeline("Before encrypting", str);
+
+	for (int i=0; i<length; i+= 8)
+	{
+		Des_Run(str+i, str+i, ENCRYPT);
+	}
+	writeline("After  encrypting", str);
+
+	for (int i=0; i<length; i+= 8)
+	{
+		Des_Run(str+i, str+i, DECRYPT);
+	}
+	writeline("After  decrypting", str);
+
+	return 0;
+}
---
+++ codebackup/recursion_partition_bak/about.md
@@ -0,0 +1,21 @@
+About DES Code Source
+=====================
+ Author : huangjinqiang  
+  Email : hellojinqiang@gmail.com   
+   Date : 2014-11-5  
+Compiler: gcc version 4.9.1  
+
+Usage:
+	
+	ssh public@117.78.8.149
+	# password: 123456
+
+Expiration date:
+
+	2014-11-27
+
+If you are logged in this machine, you need to do:
+
+	public(^_^)@JiangCentOS(~)$ cd recursion_partition
+	public(^_^)@JiangCentOS(des)$ ls
+	
